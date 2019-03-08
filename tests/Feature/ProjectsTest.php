8777a9fec20b9a37b4268cf84bcfcc855fb17212
lgramirez <gramirez.developer@gmail.com>
2019-03-08 00:08:05 -0400

    second video code

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -6,17 +6,26 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+
 class ProjectsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use WithFaker, RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+
+    public function a_user_can_create_a_project(){
+
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('projects')->assertSee($attributes['title']);
     }
 }
